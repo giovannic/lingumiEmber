65735b8c0d97c4c23b1e807d396061e91a9a8522
Giovanni Charles <giovanni.charles@gmail.com>
2016-01-19 09:15:16 +0000

    italian strings compiler

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+/*
+ * This code takes the pseudo-translations and coalesces them into an easy JSON
+ * for the i18n library to read
+ */
+module.exports = function(grunt) {
+  grunt.initConfig({
+    po2json: {
+      options: {
+        format: 'jed',
+        pretty: true,
+        singleFile: true
+      },
+      pseudo: {
+        src: ['translations/pseudo/*.po'],
+        dest: 'public/translations'
+      },
+      it: {
+        src: ['translations/it/*.po'],
+        dest: 'public/it'
+      }
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-po2json');
+
+  grunt.registerTask('default', [
+    'po2json:pseudo', 
+    'po2json:it'
+  ]);
+};
